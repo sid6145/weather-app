a1bf95dd878f28154272e49e4b98e1523e345f4d
sid6145 <sidtdesh@gmail.com>
2021-08-02 15:13:42 +0530

    moved state from weather component to home component

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import Weather from '../Weather'
 import './style.css'
+import axios from 'axios'
 
 function Home() {
+    const [weather, setWeather] = useState([])
+
+    useEffect( async () => {
+        let response = await axios.get('http://api.weatherstack.com/current?access_key=9be9650123c2e3656d516e262ecefac9&query=Mumbai')
+        let apiResponse = response.data
+        setWeather(apiResponse)
+    }, [])
+
     return (
         <div className="home-container">
             <Weather />
--- src/Components/Weather/index.js
+++ src/Components/Weather/index.js
@@ -1,22 +1,14 @@
 import React,{useState, useEffect} from 'react'
-import axios from 'axios'
+
 
 
 function Weather() {
-    const [weather, setWeather] = useState([])
 
-useEffect( async () => {
-    let response = await axios.get('http://api.weatherstack.com/current?access_key=9be9650123c2e3656d516e262ecefac9&query=Mumbai')
-    let apiResponse = response.data
-    setWeather(apiResponse)
-}, [])
-
-console.log(weather)
 
     return (
         
       <div>
-        <h1>Current temperature in mumbai { weather.current.temperature}</h1>
+    
       </div>
         
     )
