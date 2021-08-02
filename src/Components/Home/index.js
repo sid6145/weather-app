e31911e110d1098ed678189ad66c008dafc04d2f
sid6145 <sidtdesh@gmail.com>
2021-08-02 15:58:04 +0530

    implemented material ui card

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,22 +1,29 @@
-import React,{useState, useEffect} from 'react'
-import Weather from '../Weather'
-import './style.css'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import Weather from "../Weather";
+import "./style.css";
+import axios from "axios";
 
 function Home() {
-    const [weather, setWeather] = useState([])
+  const [weather, setWeather] = useState([]);
 
-    useEffect( async () => {
-        let response = await axios.get('http://api.weatherstack.com/current?access_key=9be9650123c2e3656d516e262ecefac9&query=Mumbai')
-        let apiResponse = response.data
-        setWeather(apiResponse)
-    }, [])
+  useEffect(async () => {
+    let response = await axios.get(
+      "http://api.weatherstack.com/current?access_key=9be9650123c2e3656d516e262ecefac9&query=Mumbai"
+    );
+    setWeather(response.data)
+  }, []);
 
-    return (
-        <div className="home-container">
-            <Weather />
-        </div>
-    )
+ console.log(weather.location.name)
+
+  return (
+    <div className="home-container">
+      <Weather
+        name={weather.location.name}
+        temp={weather.current.temperature}
+        description={weather.current.weather_description}
+      />
+    </div>
+  );
 }
 
-export default Home 
+export default Home;
--- src/Components/Weather/index.js
+++ src/Components/Weather/index.js
@@ -1,17 +1,43 @@
-import React,{useState, useEffect} from 'react'
+import react from "react";
+import {Card, CardActionArea, CardMedia, CardContent, CardActions, Typography, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+    root: {
+        maxWidth: 345,
+      },
+}))
 
 
+function Weather(props) {
+    const classes = useStyles()
 
-function Weather() {
-
-
-    return (
-        
-      <div>
-    
-      </div>
-        
-    )
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="h2">
+              {}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+             
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions></CardActions>
+      </Card>
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
