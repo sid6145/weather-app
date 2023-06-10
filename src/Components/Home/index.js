4044815bfa72d3e8624e0bc611df934c5996c2cb
Siddhant  Deshmukh - LFI <sdeshmukh@linkfields.com>
2023-06-10 22:18:45 +0530

    Added apikey in .env variable

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -6,14 +6,13 @@ function Home() {
   const [weather, setWeather] = useState([]);
   useEffect(() => {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&appid=8f7c13c7d5f2756dae2211f3beaca3e4"
+      `https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
     )
       .then((res) => res.json())
       .then((json) => setWeather(json));
   }, []);
 
-console.log(weather)
-
+console.log(process.env.API_ID)
   return (
     <div className="home-container">
       <video autoPlay loop muted id="video">
@@ -21,9 +20,9 @@ console.log(weather)
       </video>
 
       <Weather
-        name={weather.name}
-        temp={weather.main.temp}
-        description={weather.weather[0].description}
+        name={weather?.name || ""}
+        temp={weather?.main?.temp || ""}
+        description={weather?.weather?.length ? weather?.weather[0]?.description : ""}
       />
     </div>
   );
